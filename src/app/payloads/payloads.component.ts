ae10c11b8ca01561bb540f44e3cad202ab30f945
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-11-29 13:27:47 +0100

    clean up

--- src/app/payloads/payloads.component.ts
+++ src/app/payloads/payloads.component.ts
@@ -13,12 +13,7 @@ export class PayloadsComponent implements OnInit {
   pastLaunches$
   payloads$
 
-  constructor(private payloadsService: PayloadsGQL, private pastLaunchesServices: PastLaunchesListGQL, private readonly route: ActivatedRoute) {
-    console.log(`Am I called?`)
-    this.pastLaunches$ = this.pastLaunchesServices.fetch({ limit: 30 }).pipe(
-      tap((pastLaunches) => console.log(pastLaunches.data)),
-      map(res => res.data.launchesPast),
-    )
+  constructor(private payloadsService: PayloadsGQL) {
     this.payloads$ = this.payloadsService.fetch({ limit: 30 }).pipe(
       tap((payloads) => console.log(payloads)),
       map((payloads) => payloads.data.payloads)
